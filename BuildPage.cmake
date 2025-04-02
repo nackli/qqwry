225f9276482ba4dc3144b87419caa0b793686b0c
nackli <nackli@163.com>
2025-04-02 10:48:19 +0800

    添加作者声明

--- BuildPage.cmake
+++ BuildPage.cmake
@@ -1,3 +1,11 @@
+# @author nackli <nackli@163.com>
+# @version 1.0
+# @copyright 2025 Your Company Name. All rights reserved.
+# @License: MIT (https://opensource.org/licenses/MIT)
+# @Created: 2025-03-20
+#
+
+
 function(use_pack_fun PACKAGE_NAME PAGE_TYPE)
     message("build page = ${PACKAGE_NAME}")
     file(COPY "${CMAKE_SOURCE_DIR}/qqwry.dat" DESTINATION "${CMAKE_BINARY_DIR}/qqwry.datc")
--- QqwryParse.cpp
+++ QqwryParse.cpp
@@ -2,7 +2,8 @@
  * @author nackli <nackli@163.com>
  * @version 1.0
  * @copyright 2025 Your Company Name. All rights reserved.
- * @license MIT
+ * @License: MIT (https://opensource.org/licenses/MIT)
+ * @Created: 2025-03-20
  */
 
 #include "QqwryParse.h"
